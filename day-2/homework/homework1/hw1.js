31a4e3803b1d93e913a0b5c1c3dabcbf707bcfa6
user1 <62304291+Qrisno@users.noreply.github.com>
2020-11-13 22:18:18 +0400

    feat(hw1&2)

---
+++ day-2/homework/homework1/hw1.js
@@ -0,0 +1,5 @@
+let array1 = [2, 5, 9, 15, 0, 4];
+for (val of array1) {
+    if (val > 3 && val < 10) console.log(val);
+
+}
---
+++ day-2/homework/homework2/h2.js
@@ -0,0 +1,7 @@
+let n = 1000;
+let num = 0;
+while (n > 50) {
+    n /= 2;
+    num++;
+}
+console.log(`Iterates through the loop ${num} times !`);
